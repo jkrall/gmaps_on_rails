7aa76c047785e09d0c54141a2c9920bdec93dbf5
Joshua Krall <josh@transfs.com>
2009-06-21 22:17:04 -0500

    Revised marker management.  Markers now stored in map_markers Object, rather than each having a unique variable. This allows us to call @map.clear_markers_js and @map.add_markers_js in an RJS template, to refresh the marker positions.

--- lib/google_map.rb
+++ lib/google_map.rb
@@ -8,7 +8,7 @@ class GoogleMap
                 :zoom,
                 :api_key,
                 :javascript_framework
-  
+
   def initialize(options = {})
     self.dom_id = 'google_map'
     self.api_key = GOOGLE_APPLICATION_ID
@@ -16,51 +16,44 @@ class GoogleMap
     self.controls = [ :zoom, :overview, :scale, :type ]
     options.each_pair { |key, value| send("#{key}=", value) }
   end
-  
+
   def to_html
     html = []
-    
+
     html << "<script src='http://maps.google.com/maps?file=api&amp;v=2&amp;key=#{api_key}' type='text/javascript'></script>"
-    
-    html << "<script type=\"text/javascript\">\n/* <![CDATA[ */\n"  
+
+    html << "<script type=\"text/javascript\">\n/* <![CDATA[ */\n"
     html << to_js
     html << "/* ]]> */</script> "
-    
+
     return html.join("\n")
   end
 
   def to_js
     js = []
-    
+
     # Initialise the map variable so that it can externally accessed.
     js << "var #{dom_id};"
-    markers.each { |marker| js << "var #{marker.dom_id};" }
-    
-    js << markers_functions_js
-    
+    js << "var #{dom_id}_markers = new Object();"
+
     js << center_on_markers_function_js
-    
+
     js << "function initialize_google_map_#{dom_id}() {"
     js << "  if(GBrowserIsCompatible()) {"
     js << "    #{dom_id} = new GMap2(document.getElementById('#{dom_id}'));"
-    
+
     js << '    ' + controls_js
-    
-    js << '    ' + center_on_markers_js
-    
     js << '    ' + markers_icons_js
-    
-    # Put all the markers on the map.
-    for marker in markers
-      js << '    ' + marker.to_js
-      js << ''
-    end
-    
+
+    js << add_markers_js.gsub("\n", "    \n")
+
+    js << '    ' + center_on_markers_js
+
     js << '    ' + inject_on_load.gsub("\n", "    \n") if inject_on_load
-    
+
     js << "  }"
     js << "}"
-    
+
     # Load & unload the map with the window
     case javascript_framework
     when :jquery
@@ -74,28 +67,28 @@ class GoogleMap
       js << "  window.onload = initialize_google_map_#{dom_id};"
       js << "} else {"
       js << "  old_before_google_map_#{dom_id} = window.onload;"
-      js << "  window.onload = function() {" 
+      js << "  window.onload = function() {"
       js << "    old_before_google_map_#{dom_id}();"
-      js << "    initialize_google_map_#{dom_id}();" 
+      js << "    initialize_google_map_#{dom_id}();"
       js << "  }"
       js << "}"
       js << "if (typeof window.onunload != 'function') {"
       js << "  window.onunload = GUnload;"
       js << "} else {"
       js << "  old_before_onunload = window.onload;"
-      js << "  window.onunload = function() {" 
+      js << "  window.onunload = function() {"
       js << "    old_before_onunload();"
-      js << "    GUnload();" 
+      js << "    GUnload();"
       js << "  }"
       js << "}"
     end
 
     return js.join("\n")
   end
-  
+
   def controls_js
     js = []
-    
+
     controls.each do |control|
       case control
         when :large, :small, :overview
@@ -109,81 +102,81 @@ class GoogleMap
       end
       js << "#{dom_id}.addControl(new #{c}());"
     end
-    
+
     return js.join("\n")
   end
-  
-  def markers_functions_js
-    js = []
-    
-    for marker in markers
-      js << marker.open_info_window_function
-    end
-    
-    return js.join("\n")
-  end
-  
+
   def markers_icons_js
     icons = []
-    
+
     for marker in markers
       if marker.icon and !icons.include?(marker.icon)
-        icons << marker.icon 
+        icons << marker.icon
       end
     end
-    
+
     js = []
-    
+
     for icon in icons
       js << icon.to_js
     end
-    
+
     return js.join("\n")
   end
-  
+
+  def clear_markers_js
+    js = []
+    js << "  for(marker in #{dom_id}_markers) {"
+    js << "    GEvent.clearListeners(#{dom_id}_markers[marker], 'click');"
+    js << "  }"
+    js << "#{dom_id}.clearOverlays();"
+    js << "#{dom_id}_markers = new Object();"
+    return js.join("\n")
+  end
+
+  # Creates a JS function that removes all markers, and adds new ones
+  def add_markers_js
+    js = []
+    for marker in markers
+      js << '    ' + marker.to_js
+      js << ''
+    end
+    return js.join("\n")
+  end
+
   # Creates a JS function that centers the map on its markers.
   def center_on_markers_function_js
-    unless self.zoom and self.center
-      return "" if markers.size == 0
+    js = []
+    js << "function centerzoom_#{dom_id}(center, zoom) {"
 
-      for marker in markers
-        min_lat = marker.lat if !min_lat or marker.lat < min_lat
-        max_lat = marker.lat if !max_lat or marker.lat > max_lat
-        min_lng = marker.lng if !min_lng or marker.lng < min_lng
-        max_lng = marker.lng if !max_lng or marker.lng > max_lng
-      end
-    end
+    # Calculate the bounds of the markers
+    js << "  if (!center && !zoom) {"
+    js << "    var bounds = new GLatLngBounds();"
+    js << "    for(marker in #{dom_id}_markers) {"
+    js << "      bounds.extend(#{dom_id}_markers[marker].getLatLng());"
+    js << "    }"
+    js << "  }"
+    js << "  if (!center) { center = bounds.getCenter(); }"
+    js << "  if (!zoom) { zoom = #{dom_id}.getBoundsZoomLevel(bounds); }"
 
-    if self.zoom
-      zoom_js = zoom
-    else
-      bounds_js = "new GLatLngBounds(new GLatLng(#{min_lat}, #{min_lng}), new GLatLng(#{max_lat}, #{max_lng}))"
-      zoom_js = "#{dom_id}.getBoundsZoomLevel(#{bounds_js})"
-    end
-    
-    if self.center
-      center_js = "new GLatLng(#{self.center[0]}, #{self.center[1]})"
-    else
-      center_js = "new GLatLng(#{(min_lat + max_lat) / 2}, #{(min_lng + max_lng) / 2})"
-    end
-    set_center_js = "#{dom_id}.setCenter(#{center_js}, #{zoom_js});"
-    
-    return "function center_#{dom_id}() {\n  #{check_resize_js}\n  #{set_center_js}\n}"
+    js << "  #{dom_id}.checkResize();"
+    js << "  #{dom_id}.setCenter(center, zoom);"
+    js << "}"
+
+    return js.join("\n")
   end
-  
-  def check_resize_js
-    return "#{dom_id}.checkResize();"
-  end
-  
+
   def center_on_markers_js
-    return "center_#{dom_id}();"
+    _center = self.center ? "new GLatLng(#{self.center[0]}, #{self.center[1]})" : "false"
+    _zoom = self.zoom ? "#{self.zoom}" : "false"
+    return "centerzoom_#{dom_id}(#{_center},#{_zoom});"
   end
-  
+
   def div(width = '100%', height = '100%')
     styles = []
     styles << "width: #{width}" unless width.nil?
     styles << "height: #{height}" unless height.nil?
-    style_attr = " style='#{styles.join('; ')}'" unless styles.empty? 
+    style_attr = " style='#{styles.join('; ')}'" unless styles.empty?
     "<div id='#{dom_id}'#{style_attr}></div>"
   end
 end
